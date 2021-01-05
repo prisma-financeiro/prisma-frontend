34965c3323868fbd5a57c74140c2cc1e38d4b214
Daniel Felipe Klotz <danielfelipe.klotz@gi-de.com>
2021-01-05 15:11:40 +0100

    removed commented code

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -27,7 +27,6 @@ const SideBar: React.FC<SideBarProps> = ({ sideBarOptions }) => {
         {sideBarOptions.map((option: SideBarOption, index: number) => {
           return (
             <React.Fragment key={option.title}>
-              {/* {index > 0 && (<Divider />)} */}
               <p>{option.title}</p>
               {option.items.map((item: SideBarItem) => (<Accordion key={item.name} icon={item.icon} sectionName={item.name} expand={item.expand} onClick={item.onClick} />))}
             </React.Fragment>
