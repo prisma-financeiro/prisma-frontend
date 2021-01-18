17980a256229fe25dfa48ff089858c6bf11a01dc
Daniel Felipe Klotz <danielfelipe.klotz@gi-de.com>
2021-01-18 23:02:07 +0100

    Included protected routes and changed public routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import Main from '../pages/Main';
+import AssetsCompare from '../pages/AssetsCompare';
+import Ranking from '../pages/Ranking';
+import Company from '../pages/Company';
+import AssetsExplorer from '../pages/AssetsExplorer';
+import Login from '../pages/Login';
+
+
+const Routes = () => {
+  
+  return (
+    <Switch>
+        <PrivateRoute path="/home" exact component={Main} />
+        <PrivateRoute path="/assets-compare" exact component={AssetsCompare} />
+        <PrivateRoute path="/ranking" exact component={Ranking} />
+        <PrivateRoute path="/company/:id/:ticker" component={Company}/>
+        <PrivateRoute path="/assets-explorer" exact component={AssetsExplorer} />
+        <Route path="/" component={Login} />
+
+        <Route render={() => <Redirect to="/home" />} />
+    </Switch>
+  );
+}
+
+export default Routes;
