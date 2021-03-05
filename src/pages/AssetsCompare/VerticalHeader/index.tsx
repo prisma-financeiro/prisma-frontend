d3254fb4a83591d32934dce6d6ee5b6801c43322
Daniel Klotz <danielfelipeklotz@gmail.com>
2021-03-05 11:01:12 +0100

    correcting misspeling

--- src/pages/AssetsCompare/VerticalHeader/index.tsx
+++ src/pages/AssetsCompare/VerticalHeader/index.tsx
@@ -38,7 +38,7 @@ const VerticalHeader: React.FC<VerticalHeaderProps> = ({ anchor, openModal }) =>
       </AssetHeader>
 
       <div ref={anchor.valuation}>
-        <Title thinnerHight={true}>
+        <Title thinnerHeight={true}>
           Valuation
         </Title>
         {valuationFieldNames.map((field, index) => <Field key={index}>{field}</Field>)}
--- src/pages/AssetsCompare/VerticalHeader/styles.ts
+++ src/pages/AssetsCompare/VerticalHeader/styles.ts
@@ -8,10 +8,10 @@ export const Container = styled.div`
   `}
 `;
 
-export const Title = styled.div<{thinnerHight?: boolean}>`
-  ${({ theme, thinnerHight }) => css`
+export const Title = styled.div<{thinnerHeight?: boolean}>`
+  ${({ theme, thinnerHeight }) => css`
     width: 19rem;
-    height: ${thinnerHight ? '3rem' : '5rem'};
+    height: ${thinnerHeight ? '3rem' : '5rem'};
     display: flex;
     padding-bottom: 0.5rem;
     align-items: flex-end;
